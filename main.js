2e4a8607ee87c3df24698158a5ae15a83ba7159e
Gabriel Moraga <agarom97@gmail.com>
2021-01-27 16:54:41 +0100

    Bonus, aggiunta animazione farlocca

--- main.js
+++ main.js
@@ -6,21 +6,42 @@ var app = new Vue({
     'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
     'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'],
     imgIndex: 0,
+    imgSlideTransition: ''
   },
   methods: {
     next: function() { // Bottoni prev e next per lo slider
+      this.imgSlideTransition = 'transform: translateX(-100vw); transition: all .3s ease;';
+
       if (this.imgIndex >= this.imgs.length - 1) {
         this.imgIndex = 0;
       }else {
         this.imgIndex += 1;
       }
+
+      setTimeout(() => {
+        this.imgSlideTransition = 'transform: translateX(100vw);';
+      }, 200)
+
+      setTimeout(() => {
+        this.imgSlideTransition = 'transition: all .3s ease;;';
+      }, 250)
     },
     prev: function() {
+      this.imgSlideTransition = 'transform: translateX(100vw); transition: all .3s ease;';
+
       if (this.imgIndex <= 0) {
         this.imgIndex = this.imgs.length - 1;
       }else {
         this.imgIndex -= 1;
       }
+
+      setTimeout(() => {
+        this.imgSlideTransition = 'transform: translateX(-100vw);';
+      }, 200)
+
+      setTimeout(() => {
+        this.imgSlideTransition = 'transition: all .3s ease;;';
+      }, 250)
     },
     indexBoleean: function(index) { //funzione che permette il spostarsi dell'active dei pallini di .nav
       if (index === this.imgIndex) {
